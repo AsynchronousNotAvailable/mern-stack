50276790a7d4ff8dd1c523717aeae938baeb4580
Weng Hong :D <bobosgagas@hotmail.com>
2022-08-20 23:40:49 +0800

    updated

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react"
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            //fetch data from server 
+            const response = await fetch('/api/workouts');
+            // parse json into the response object
+            const json = await response.json();
+            // if succeed, the json should contain workout object
+            // check if response is ok
+            if (response.ok) {
+                setWorkouts(json)
+            }
+            
+
+        }
+        fetchWorkouts()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((i) => (
+                    <WorkoutDetails key={i._id} workout={i}/>
+                ))}
+
+                <WorkoutForm/>
+            </div>
         </div>
     )
 }
